7aac72c199a9e9e0ff84e65ef8cd7521fcd96d4b
xyztai <taixyz@163.com>
2021-07-29 13:07:23 +0800

    Update IndexController.java

--- spring-boot-demo/src/main/java/com/javat/controller/IndexController.java
+++ spring-boot-demo/src/main/java/com/javat/controller/IndexController.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
     @GetMapping("")
     public String index(){
-        return "Welcome Here!";
+        return "Welcome spring-boot-demo!";
     }
 }
