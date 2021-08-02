e2453d26000e9bbfe21360b955ee6bcee06aa4fd
xyztai <taixyz@163.com>
2021-08-02 23:59:00 +0800

    增加一个spring-boot-regstry-demo

---
+++ nacos-demo/nacos-spring-boot-registry-demo/src/main/java/com/regist/nacos/config/NacosConfig.java
@@ -0,0 +1,29 @@
+package com.regist.nacos.config;
+
+import com.alibaba.nacos.api.annotation.NacosInjected;
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.api.naming.NamingService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * @Author t
+ * @create 2021-08-02 22:38
+ */
+@Configuration
+public class NacosConfig {
+    @Value("${server.port}")
+    private int serverPort;
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @NacosInjected
+    private NamingService namingService;
+
+    @PostConstruct
+    public void registerInstance() throws NacosException {
+        namingService.registerInstance(applicationName, "127.0.0.1", serverPort);
+    }
+}
