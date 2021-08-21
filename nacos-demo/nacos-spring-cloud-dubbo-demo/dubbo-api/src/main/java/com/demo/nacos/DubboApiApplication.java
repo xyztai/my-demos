ad12d882a5a7ed84180fee908d9f553e3ddfd26d
xyztai <taixyz@163.com>
2021-08-21 12:09:18 +0800

    add new files

---
+++ nacos-demo/nacos-spring-cloud-dubbo-demo/dubbo-api/src/main/java/com/demo/nacos/DubboApiApplication.java
@@ -0,0 +1,15 @@
+package com.demo.nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author t
+ * @create 2021-08-21 11:51
+ */
+@SpringBootApplication
+public class DubboApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DubboApiApplication.class, args);
+    }
+}
---
+++ nacos-demo/nacos-spring-cloud-dubbo-demo/dubbo-api/src/main/java/com/demo/nacos/nscdd/service/IHelloService.java
@@ -0,0 +1,9 @@
+package com.demo.nacos.nscdd.service;
+
+/**
+ * @Author t
+ * @create 2021-08-21 12:06
+ */
+public interface IHelloService {
+    String hello(String name);
+}
