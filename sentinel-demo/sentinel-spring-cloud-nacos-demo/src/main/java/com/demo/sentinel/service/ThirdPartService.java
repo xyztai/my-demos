853f36edf9542b011ab138241e54e5df15b96df4
xyztai <taixyz@163.com>
2021-08-26 14:02:23 +0800

    add a new java

---
+++ sentinel-demo/sentinel-spring-cloud-nacos-demo/src/main/java/com/demo/sentinel/service/ThirdPartService.java
@@ -0,0 +1,19 @@
+package com.demo.sentinel.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author tai
+ * @create 2021-08-26 13:51
+ */
+@Service
+public class ThirdPartService {
+
+    @Autowired
+    private IProviderService providerService;
+
+    public String getEcho(String name){
+        return "providerService " + providerService.echo(name);
+    }
+}
